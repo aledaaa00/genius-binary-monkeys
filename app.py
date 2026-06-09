e15d89c6f1d0955b64de99478e919cab7b7e0a1d
Cosi13 <cosntulmeu2@gmail.com>
2026-06-09 16:12:00 -0400

    basic template

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/earthquakes')
+def earthquakes():
+    return render_template('earthquakes.html')
+
+@app.route('/wildfires')
+def wildfires():
+    return render_template('wildfires.html')
+
+@app.route('/storms')
+def storms():
+    return render_template('storms.html')
+
+@app.route('/floods')
+def floods():
+    return render_template('floods.html')
+
+
+@app.route('/extreme-heat')
+def extreme_heat():
+    return render_template('extreme_heat.html')
+
+@app.route('/kit')
+def kit():
+
+
+    return render_template('kit.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
